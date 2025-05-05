9d5344f4fdac03273ff083b872cc5bd4d7bb0e0b
Lucas Coelho <lucascoelhodacosta@gmail.com>
2025-05-05 17:36:23 -0300

    chore: move game -> index

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ scripts/build.sh # build the project
 python3 -m http.server
 ```
 
-Then access http://localhost:8000/game.html to see the project running.
+Then access http://localhost:8000/ to see the project running.
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,1 @@
-emcc -o game.html game.c -Os -Wall ./vendor/libraylib.a -I. -I./vendor/raylib.h -L. -L./vendor/libraylib.a -s USE_GLFW=3 --shell-file shell.html -DPLATFORM_WEB
+emcc -o index.html game.c -Os -Wall ./vendor/libraylib.a -I. -I./vendor/raylib.h -L. -L./vendor/libraylib.a -s USE_GLFW=3 --shell-file shell.html -DPLATFORM_WEB
